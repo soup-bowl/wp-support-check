d4c7f5c8ee39b356f2a5a45855a04d00fd01fe72
soup-bowl <code@soupbowl.io>
2020-08-21 22:42:45 +0100

    Split up code.

--- wp-support-check.php
+++ wp-support-check.php
@@ -52,27 +52,47 @@ class Sb_Security_Check {
 	public function plugin_table_content( $column_name, $plugin_file, $plugin_data ) {
 		if ( 'wpsc_status' === $column_name ) {
 			if ( isset( $plugin_data['slug'] ) ) {
-				$payload = wp_remote_get( "https://api.wordpress.org/plugins/info/1.0/{$plugin_data['slug']}.json" );
-				if ( ! is_wp_error( $payload ) && 200 === wp_remote_retrieve_response_code( $payload ) ) {
-					$plugin_details   = json_decode( wp_remote_retrieve_body( $payload ) );
-					$last_update_date = DateTime::createFromFormat( 'Y-m-d', substr( $plugin_details->last_updated, 0, 10 ) );
+				$check_wp_site = $this->check_wordpress_directory( $plugin_data['slug'] );
 
-					$days_since_update = $last_update_date->diff( new DateTime() )->days;
-					if ( $last_update_date->diff( new DateTime() )->days > 365 ) {
-						$years = $this->days_to_years( $days_since_update );
-						$label = ( 1 === $years ) ? 'year' : 'years';
-
-						echo esc_html( Sb_Support_Emoji::NEGATIVE . " No updates in {$years} {$label}." );
+				if ( $check_wp_site['success'] ) {
+					if ( $check_wp_site['age'] > 1 ) {
+						$label = ( 1 === $check_wp_site['age'] ) ? 'year' : 'years';
+						echo esc_html( Sb_Support_Emoji::NEGATIVE . " No updates in {$check_wp_site['age']} {$label}." );
 					} else {
 						echo esc_html( Sb_Support_Emoji::POSITIVE );
 					}
-					// echo '<pre>';var_dump( $plugin_details );echo '</pre>';.
 				} else {
-					echo esc_html( Sb_Support_Emoji::QUERY . ' Plugin not found on the WordPress directory.' );
+					echo esc_html( $check_wp_site['message'] );
 				}
 			} else {
 				echo esc_html( Sb_Support_Emoji::QUERY . ' Plugin information not found.' );
 			}
+		}
+	}
+
+	/**
+	 * Gets a collection of analysis information from the WordPress directory.
+	 *
+	 * @param string $slug WordPress plugin identifier.
+	 * @return array 'success' boolean. 'message' upon fail.
+	 */
+	private function check_wordpress_directory( $slug ) {
+		$payload = wp_remote_get( "https://api.wordpress.org/plugins/info/1.0/{$slug}.json" );
+		if ( ! is_wp_error( $payload ) && 200 === wp_remote_retrieve_response_code( $payload ) ) {
+			$plugin_details   = json_decode( wp_remote_retrieve_body( $payload ) );
+			$last_update_date = DateTime::createFromFormat( 'Y-m-d', substr( $plugin_details->last_updated, 0, 10 ) );
+
+			$days_since_update = $last_update_date->diff( new DateTime() )->days;
+			return [
+				'success' => true,
+				'age'     => $this->days_to_years( $days_since_update ),
+			];
+			// echo '<pre>';var_dump( $plugin_details );echo '</pre>';.
+		} else {
+			return [
+				'success' => false,
+				'message' => Sb_Support_Emoji::QUERY . ' Plugin not found on the WordPress directory.',
+			];
 		}
 	}
 
