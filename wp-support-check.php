d09be8b5cadc7f9c6db1845829a9405c99284826
soup-bowl <code@soupbowl.io>
2020-08-21 22:21:09 +0100

    Early project code improevements.

--- wp-support-check.php
+++ wp-support-check.php
@@ -17,19 +17,40 @@
  * Text Domain:       wpsecuritycheck
  */
 
-class sb_security_check {
+/**
+ * Main operatioal class.
+ */
+class Sb_Security_Check {
+	/**
+	 * Inform the WordPress system of our plugin functionality.
+	 */
 	public function register() {
-		add_filter( 'manage_plugins_columns', array( &$this, 'wpa65189_add_plugins_column' ) );
-		add_action( 'manage_plugins_custom_column' , array( &$this, 'wpa65189_render_plugins_column' ), 10, 3 );
+		add_filter( 'manage_plugins_columns', array( &$this, 'plugin_table_columns' ) );
+		add_action( 'manage_plugins_custom_column', array( &$this, 'plugin_table_content' ), 10, 3 );
 	}
 
-	public function wpa65189_add_plugins_column( $columns ) {
+	/**
+	 * Register the plugin column.
+	 *
+	 * @param string[] $columns Column names.
+	 * @return string[]
+	 */
+	public function plugin_table_columns( $columns ) {
 		$columns['wpsc_status'] = 'Security Check';
+
 		return $columns;
 	}
 
-	public function wpa65189_render_plugins_column( $column_name, $plugin_file, $plugin_data ) {
-		if ( 'wpsc_status' == $column_name ) {
+	/**
+	 * Process the response displayed in the plugin column.
+	 *
+	 * @param string $column_name Processed column name.
+	 * @param string $plugin_file The location of the plugin operational file.
+	 * @param array  $plugin_data Information about the currently processed plugin.
+	 * @return void Function prints to the page.
+	 */
+	public function plugin_table_content( $column_name, $plugin_file, $plugin_data ) {
+		if ( 'wpsc_status' === $column_name ) {
 			if ( isset( $plugin_data['slug'] ) ) {
 				$payload = wp_remote_get( "https://api.wordpress.org/plugins/info/1.0/{$plugin_data['slug']}.json" );
 				if ( ! is_wp_error( $payload ) && 200 === wp_remote_retrieve_response_code( $payload ) ) {
@@ -37,33 +58,42 @@ class sb_security_check {
 					$last_update_date = DateTime::createFromFormat( 'Y-m-d', substr( $plugin_details->last_updated, 0, 10 ) );
 
 					$days_since_update = $last_update_date->diff( new DateTime() )->days;
-					if( $last_update_date->diff( new DateTime() )->days > 365 ) {
+					if ( $last_update_date->diff( new DateTime() )->days > 365 ) {
 						$years = $this->days_to_years( $days_since_update );
 						$label = ( 1 === $years ) ? 'year' : 'years';
 
-						echo sb_support_emoji::NEGATIVE . " No updates in {$years} {$label}.";
+						echo esc_html( Sb_Support_Emoji::NEGATIVE . " No updates in {$years} {$label}." );
 					} else {
-						echo sb_support_emoji::POSITIVE;
+						echo esc_html( Sb_Support_Emoji::POSITIVE );
 					}
-					#echo '<pre>';var_dump( $plugin_details );echo '</pre>';
+					// echo '<pre>';var_dump( $plugin_details );echo '</pre>';.
 				} else {
-					echo sb_support_emoji::QUERY . ' Plugin not found on the WordPress directory.';
+					echo esc_html( Sb_Support_Emoji::QUERY . ' Plugin not found on the WordPress directory.' );
 				}
 			} else {
-				echo sb_support_emoji::QUERY . ' Plugin information not found.';
+				echo esc_html( Sb_Support_Emoji::QUERY . ' Plugin information not found.' );
 			}
 		}
 	}
 
+	/**
+	 * Converts days to years.
+	 *
+	 * @param integer $days Numerical day reference.
+	 * @return integer
+	 */
 	private function days_to_years( $days ) {
 		return floor( $days / 365 );
 	}
 }
 
-class sb_support_emoji {
-	const POSITIVE = '✔️';
-	const NEGATIVE = '❌';
-	const QUERY    = '❓';
+/**
+ * Emojis provided by latin reference.
+ */
+class Sb_Support_Emoji {
+	const POSITIVE = "\u{2714}";
+	const NEGATIVE = "\u{274C}";
+	const QUERY    = "\u{2753}";
 }
 
-( new sb_security_check() )->register();
+( new Sb_Security_Check() )->register();
